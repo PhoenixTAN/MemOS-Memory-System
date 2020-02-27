05831b43d45a3cbd75af1304ef7482e5ec1070db
PhoenixTAN <tanziqi1756@outlook.com>
2020-02-26 22:21:43 -0500

    Get memory map.

--- ZiqiTan/Detecting Memory x86.md
+++ ZiqiTan/Detecting Memory x86.md
@@ -23,4 +23,52 @@ It is the only BIOS function that can detect memory areas above 4G. It is meant 
 
 In reality, **this function returns an unsorted list** that may contain unused entries and (in rare/dodgy cases) may return overlapping areas. Each list entry is **stored in memory at ES:DI, and DI is not incremented** for you.
 
+The format of an entry is 2 uint64_t's and a uint32_t in the 20 byte version, plus one additional uint32_t in the 24 byte ACPI 3.0 version (but nobody has ever seen a 24 byte one). It is probably best to always store the list entries as 24 byte quantities -- to preserve uint64_t alignments, if nothing else. (Make sure to set that last uint64_t to 1 before each call, to make your map compatible with ACPI).
 
+- First uint64_t = Base address
+- Second uint64_t = Length of "region" (if this value is 0, ignore the entry)
+- Next uint32_t = Region "type"
+    - Type 1: Usable (normal) RAM
+    - Type 2: Reserved - unusable
+    - Type 3: ACPI reclaimable memory
+    - Type 4: ACPI NVS memory
+    - Type 5: Area containing bad memory
+- Next uint32_t = ACPI 3.0 Extended Attributes bitfield (if 24 bytes are returned, instead of 20)
+    - Bit 0 of the Extended Attributes indicates if the entire entry should be ignored (if the bit is clear). This is going to be a huge compatibility problem because most current OSs won't read this bit and won't ignore the entry.
+    - Bit 1 of the Extended Attributes indicates if the entry is non-volatile (if the bit is set) or not. The standard states that "Memory reported as non-volatile may require characterization to determine its suitability for use as conventional RAM."
+    - The remaining 30 bits of the Extended Attributes are currently undefined.
+
+#### Notes:
+- After getting the list, it may be desirable to: sort the list, combine adjacent ranges of the same type, change any overlapping areas to the most restrictive type, and change any unrecognised "type" values to type 2.
+- Type 3 "ACPI reclaimable" memory regions may be used like (and combined with) normal "available RAM" areas as long as you're finished using the ACPI tables that are stored there (i.e. it can be "reclaimed").
+- Types 2, 4, 5 (reserved, ACPI non-volatile, bad) mark areas that should be avoided when you are allocating physical memory.
+Treat unlisted regions as Type 2 -- reserved.
+- Your code must be able to handle areas that don't start or end on any sort of "page boundary".
+
+#### Output sample:
+```
+Base Address       | Length             | Type  
+0x0000000000000000 | 0x000000000009FC00 | Free Memory (1)  
+0x000000000009FC00 | 0x0000000000000400 | Reserved Memory (2)  
+0x00000000000E8000 | 0x0000000000018000 | Reserved Memory (2)  
+0x0000000000100000 | 0x0000000001F00000 | Free Memory (1)  
+0x00000000FFFC0000 | 0x0000000000040000 | Reserved Memory (2)  
+```
+
+### Basic usage
+- For the first call to the function, point **ES:DI** at the destination buffer for the list. 
+- Clear EBX. 
+- Set EDX to the magic number 0x534D4150. 
+- Set EAX to 0xE820 (note that the upper 16-bits of EAX should be set to 0). 
+- Set ECX to 24. 
+- Do an INT 0x15.
+- If the first call to the function is successful, EAX will be set to 0x534D4150, and the Carry flag will be clear. 
+- EBX will be set to some non-zero value, which must be preserved for the next call to the function. 
+- CL will contain the number of bytes actually stored at ES:DI (probably 20).
+- For the subsequent calls to the function: increment DI by your list entry size, reset EAX to 0xE820, and ECX to 24. 
+- When you reach the end of the list, EBX may reset to 0. 
+- If you call the function again with EBX = 0, the list will start over. 
+- If EBX does not reset to 0, the function will return with Carry set when you try to access the entry after the last valid entry.
+
+### Sample code
+![alt text](./image/INT15-E820-Sample-code.png)
--- ZiqiTan/memos1/memos1.s
+++ ZiqiTan/memos1/memos1.s
@@ -29,6 +29,8 @@ _start:
 
     call print_Welcome
 	call print_line
+	call getAddressMap
+	call printAddressMap
     jmp .
 # -------------------------------------------------------------------------
 
@@ -49,13 +51,6 @@ print_Welcome:
     
 	ret
 
-    welcome_string: 	  .ascii "MemOS 1: Welcome *** System Memory is: 0x"
-    # .ascii expects zero or more string literals separated by commas. 
-    # It assembles each string (with no automatic trailing zero byte) into consecutive addresses.
-
-    welcome_string_len: 	  .word  . - welcome_string    # . is the current address
-    memory_unit: .ascii " MB  "
-    len_memory_unit: .word	 . - memory_unit
 
 # -----------------------------------------------------------------------
 print_string:
@@ -113,13 +108,13 @@ get_memory_size:
 	cmpb 	$0x80, %AH
 	je		E801_ERROR			# if E801 does not work, return ;
 
-    addw 	$0x400, %ax			# ax add 1MB
-	shll 	$6, %ebx	  		# bx times 64
-	addl 	%EAX, %ebx			# ax + bx
-	shrl 	$10, %ebx			# convert KB to MB
+    addw 	$0x400, %AX			# ax add 1MB
+	shll 	$6, %EBX	  		# bx times 64
+	addl 	%EAX, %EBX			# ax + bx
+	shrl 	$10, %EBX			# convert KB to MB
 	
-	movl 	$memory_size, %esi	# put the address of memory_size in SI
-	movl	%ebx, (%esi) 		# put the answer to memory_size
+	movl 	$memory_size, %ESI	# put the address of memory_size in SI
+	movl	%EBX, (%ESI) 		# put the answer to memory_size
 								# () means that we use SI as an address
 	
 	call 	print_2_word 			# print the data in address of memory_size
@@ -132,7 +127,6 @@ memory_size: .word 0
 			 .word 0
 
 # ---------------------------------------------------------------------
-
 
 # -----------------------------------------------------------------------
 # Function: print 32-bit binary number in hexadecimal form.
@@ -155,7 +149,6 @@ do_print_32:
 	addl	$5, %esi
 	popl 	%ecx				# retrive CX
 	ret
-
 
 # -------------------------------------------------------------------------
 print_hex_digit:
@@ -181,11 +174,152 @@ do_print_hex:
 
 	ret
 
-
 # -----------------------------------------------------------
-print_heximal:
+	.set	MMARD_SIZE, 20			#memory map address range descriptor size
+	.set 	magic_number, 0x534d4150
+# eax for counter, es:si for buffer
+getAddressMap:
+	pushl	%ebp
+	pushl	%esp
+
+	# TODO: For the first call to the function, point ES:DI at the destination buffer for the list.
+	xorl	%EBX, %EBX 			# clear EBX
+	movl	$MMARD_SIZE, %ECX 	# set ECX to 24
+	movl	$magic_number, %EDX 	# magic number
+								
+	movl	$buffer, %edi 		# edi for return buffer
+	xorw	%BP, %BP 			# BP keeps an entry count
+
+	# Use INT 15, AX = E820 to get memory map
+	movl	$0xE820, %EAX 		
+	int 	$0x15
+
+	# If the first call to the function is successful,
+	# EAX will be set to magic_number,
+	# and the carry flag will be clear.	
+
+	jc		E802_ERR					# jump if CF = 1
+	cmpl	$magic_number, %EAX 	
+	jne 	E802_ERR					# jump if not equal (ZF=0)
+
+E802_next_call:
+	# EBX will be set to some non-zero value, 
+	# which must be preserved for the next call to the function.
+	test	%EBX, %EBX 			# ebx & ebx == 0
+	je 		E802_END 				# jump if equal (ZF=1)
+	
+	# For the subsequent calls to the function: 
+	# increment DI by your list entry size, 
+	# reset EAX to 0xE820, and ECX to 24.
+	# When you reach the end of the list, EBX may reset to 0.
+
+	incw	%BP				 	# increment entry count
+	addl	$MMARD_SIZE, %EDI 	# increase return address
+
+	movl	$0xe820, %EAX 		# reset EAX to 0xE820
+	movl	$MMARD_SIZE, %ECX 	
+	int 	$0x15 				# next call
+	jmp 	E802_next_call		# loop until ebx = 0
+
+E802_END:
+	xorl	%eax, %eax 			# reset
+ 	movw 	%bp, %ax 			# copy counter to ax
+ 	movl 	$buffer, %esi 		# copy address to esi
+ 	clc 						# clear CF
+ 	popl 	%esp
+ 	popl 	%ebp
+E802_ERR:
+ 	ret
+
+# -----------------------------------------------------------------------
+# es:si for begin address, eax for count
+printAddressMap:
+	pushl 	%esi
+
+entry_loop:
+	pushl	%EAX				# save counter
+
+	# print 'Memory Range ['
+	pushl	%ESI		
+	leaw 	memory_range_string, %SI
+	movw 	len_memory_range_string, %CX
+	call 	print_string		
+	popl	%ESI
+	
+	movb 	$0x03, %ah 			# int10 0x3 read cursor position
+	movb 	$0x00, %bh
+	int 	$0x10
+
+	call 	print_2_word 			# print base address
+
+	# print ':'
+	movb $':', %AL
+	movb $0x0e, %AH
+	int $0x10 					# int10 0xe print ':'
+
+	addl	$16, %esi 			# move to the base address of the next entry 
+	call 	print_2_word 			# print end address
+	
+	pushl	%esi
+	leaw 	str_type, %SI
+	movw 	len_type, %CX
+	call 	print_string 		# print '] State:'
+	popl	%esi
+
+	subl	$8, %esi 			# move back to the type of current entry
+	movl	(%esi), %eax 		# read the type into eax
+
+	pushl	%esi
+	cmp 	$0x02, %eax 		# compare to 2
+	je 		state2
+	leaw 	str_state1, %SI
+	movw 	state1_len, %CX
+	jmp 	state_con
+
+state2: 
+	leaw 	str_state2, %SI
+	movw 	state2_len, %CX
+
+state_con:
+	call 	print_string 		# print the type
+	popl	%esi
+
+	addl	$4, %esi 			# move to the begining of the next entry
+
+	call 	print_line 			
+
+	popl	%eax
+	decl 	%eax
+	cmpl 	$0, %eax 			# loop counter compare to 0
+	jge		entry_loop
+
+	popl	%esi
 
 	ret
+# -------------------------------------------------------------------------
+
+# Constant strings
+# --------------------------------------------------------------------------------------
+welcome_string: 	  .ascii "MemOS 1: Welcome *** System Memory is: 0x"
+	# .ascii expects zero or more string literals separated by commas. 
+	# It assembles each string (with no automatic trailing zero byte) into consecutive addresses.
+welcome_string_len: 	  .word  . - welcome_string    
+	# . is the current address
+
+memory_unit: .ascii " MB  "
+len_memory_unit: .word	 . - memory_unit
+
+memory_range_string: 	.ascii	"Memory Range ["
+len_memory_range_string: 	.word  	. - memory_range_string
+
+str_type: 	.ascii 	"] Type: "
+len_type: 	.word  	. - str_type
+
+str_state1:	.ascii	"Free Memory"
+state1_len:	.word	. - str_state1
+
+str_state2:	.ascii	"Reserved Memory"
+state2_len: .word	. - str_state2
 
 # This is going to be in our MBR for Bochs.
 # -------------------------------------------------------
@@ -197,5 +331,8 @@ print_heximal:
 	.byte 0xAA
 
 # ------------------------------------------------------
+# This 
 
-
+buffer:
+	.fill 	20 * MMARD_SIZE, 1 
+	.set	buffer_len, . - buffer
